2e54c348f834e12368563f09ed528d050259f323
Arav-Mathur <aravmathur007@gmail.com>
2024-07-02 14:10:42 -0400

    l

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { useState } from 'react';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <header className='App'>
+        <Mybutton/>
+        <Mybutton/> 
+        {/* by default each instance has its own states and they do not intersect */}
+      </header> {/*classname refers to the css code */}
+      
     </div>
   );
 }
 
-export default App;
+
+function Mybutton(){ // function name needs to start capatilized to be used as components
+
+  const handleClick =()=>{
+    console.log("Button was clicked!")
+    setCount(count+1)
+  } // u can use js by putting that code in curly brackets. 
+
+  const numbers = [
+    {object:1},
+    {object:2},
+    {object:3},
+  ]
+
+  const list = numbers.map((number,index)=> <li key={index}> {number.object} </li>)
+  // use map function for converting lists into renderable things
+
+  const [count, setCount] = useState(0) 
+
+  return(
+    <div>
+    <button onClick={handleClick}>clicked {count} times </button>
+    <ul>{list}</ul>
+    </div>
+  )
+}
+
+
+
